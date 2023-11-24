57016f67cf4c7063b556248b41a8d54642d87939
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-24 16:12:55 +0100

    feat: app vue - messaggio immagine e testo alt

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+// Salvare in una variabile createApp
 const { createApp } = Vue;
 
+// inizializzazione istanza
 createApp({
+  // definiamo le variabili all'interno di data
   data() {
     return {
-      message: "hello vue",
+      messaggio: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      immagine: "https://picsum.photos/id/237/200/300",
+      testoAlt: "Testo alternativo",
     };
   },
 }).mount("#app");
